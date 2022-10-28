923d3a7e6fcb7ed4f2d6e866ab51c9ac2689f30a
conor-keane <g00391846@atu.ie>
2022-10-28 18:48:29 +0100

    Added test to successfully test result of "Mr" in title feature.

--- src/main/java/ie/atu/week6/Passenger.java
+++ src/main/java/ie/atu/week6/Passenger.java
@@ -1,37 +1,34 @@
 package ie.atu.week6;
 
 public class Passenger {
+
     private String Title;
     private String Name;
-    private int id;
-    private int phoneNo;
+    private long id;
+    private long phoneNo;
     private int age;
 
-    public Passenger(String title, String name, int id, int phoneNo, int age) {
-        Title = title;
-        Name = name;
+    public Passenger(String title, String name, long id, long phoneNo, int age) {
+        this.Title = title;
+        this.Name = name;
         this.id = id;
         this.phoneNo = phoneNo;
         this.age = age;
     }
 
     public void setTitle(String title) {
+        /*
+        if(title.equals("Mr") || title.equals("Mrs") || title.equals("Miss")) {
+            Title = title;
+        }
+        else {
+            printf("Invalid entry");
+        }
+
+         */
         Title = title;
     }
-
-    public void setName(String name) {
-        Name = name;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setPhoneNo(int phoneNo) {
-        this.phoneNo = phoneNo;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    public String getTitle() {
+        return Title;
     }
 }
--- src/test/java/ie/atu/week6/PassengerTest.java
+++ src/test/java/ie/atu/week6/PassengerTest.java
@@ -7,11 +7,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassengerTest {
+    Passenger myPass;
 
     @BeforeEach
     void setUp() {
     }
 
+    @Test
+    void test_Title() {
+        Passenger myPass = new Passenger("Mr", "Conor", 12345678910L, 879543461L, 21);
+        assertEquals("Mr", myPass.getTitle());
+    }
 
     @AfterEach
     void tearDown() {
